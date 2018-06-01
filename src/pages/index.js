1fc144b557f32f802d4f084c15cad23380f6b9f4
Timo Jaaskelainen <timojaask@gmail.com>
2018-06-01 14:46:43 +0100

    Add CSS module example

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,5 @@
 import React from "react"
+import styles from "./index.module.css";
 
-export default () => <div>Hello world!</div>
+export default () =>
+  <div className={styles.container}>Hello world!</div>
